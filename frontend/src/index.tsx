855d1e8c45384455c2c166af45910fb3959f8a7c
Abraham Hernandez <abeherham@knights.ucf.edu>
2022-03-01 22:53:21 -0500

    Add missing files

---
+++ frontend/src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import './styles/index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Used to keep UI in sync with the URL, only reloading components that need to be changed instead of entire page
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Register() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <h1 className="text-5xl text-center text-green-500 font-serif">
+        Welcome! <br />
+        Everything is register
+      </h1>
+    </div>
+  );
+}
+
+export default Register;
